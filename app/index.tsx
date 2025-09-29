d42559174ff03461e45a6eb427b4dabfbf912d6b
Valentino Giudice <giudicevalentino@gmail.com>
2025-09-29 19:44:10 +0800

    refactor(home): make home screen redirect to chats screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,5 @@
-import { ThemedText } from '@/components/themed-text'
-import { ThemedView } from '@/components/themed-view'
-import { router } from 'expo-router'
-import { Button, StyleSheet } from 'react-native'
+import { Redirect } from 'expo-router'
 
 export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <ThemedText style={styles.title}>Welcome to Bonobo</ThemedText>
-      <Button
-        title="Analyze Chat"
-        onPress={() => {
-          router.push('/chats')
-        }}
-      />
-    </ThemedView>
-  )
+  return <Redirect href="/chats" />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-})
